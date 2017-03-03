ac7ef85960fbb59cd87ea9d4a7d4b6e68196ce2d
gapatprakash@gmail.com <gapatprakash@gmail.com>
2017-03-03 11:09:38 +0530

    All Material

---
+++ Prashant/Session 14 (Core ADO.Net)/DBApp/DBApp/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace DBApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnName = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.dgResult = new System.Windows.Forms.DataGridView();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblFName = new System.Windows.Forms.Label();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.lblLName = new System.Windows.Forms.Label();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.dsCust1 = new DBApp.DsCust();
+            ((System.ComponentModel.ISupportInitialize)(this.dgResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsCust1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnName
+            // 
+            this.btnName.Location = new System.Drawing.Point(101, 12);
+            this.btnName.Name = "btnName";
+            this.btnName.Size = new System.Drawing.Size(75, 23);
+            this.btnName.TabIndex = 0;
+            this.btnName.Text = "Show";
+            this.btnName.UseVisualStyleBackColor = true;
+            this.btnName.Click += new System.EventHandler(this.btnName_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(101, 41);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(155, 20);
+            this.txtResult.TabIndex = 1;
+            // 
+            // dgResult
+            // 
+            this.dgResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgResult.Location = new System.Drawing.Point(91, 177);
+            this.dgResult.Name = "dgResult";
+            this.dgResult.Size = new System.Drawing.Size(614, 373);
+            this.dgResult.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(59, 41);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(37, 13);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Result";
+            // 
+            // lblFName
+            // 
+            this.lblFName.AutoSize = true;
+            this.lblFName.Location = new System.Drawing.Point(281, 34);
+            this.lblFName.Name = "lblFName";
+            this.lblFName.Size = new System.Drawing.Size(62, 13);
+            this.lblFName.TabIndex = 5;
+            this.lblFName.Text = "Firsst Name";
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(349, 34);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(155, 20);
+            this.txtFName.TabIndex = 4;
+            // 
+            // lblLName
+            // 
+            this.lblLName.AutoSize = true;
+            this.lblLName.Location = new System.Drawing.Point(285, 72);
+            this.lblLName.Name = "lblLName";
+            this.lblLName.Size = new System.Drawing.Size(58, 13);
+            this.lblLName.TabIndex = 7;
+            this.lblLName.Text = "Last Name";
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(349, 69);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(155, 20);
+            this.txtLName.TabIndex = 6;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(307, 107);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(24, 13);
+            this.lblCity.TabIndex = 9;
+            this.lblCity.Text = "City";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(349, 107);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(155, 20);
+            this.txtCity.TabIndex = 8;
+            // 
+            // dsCust1
+            // 
+            this.dsCust1.DataSetName = "DsCust";
+            this.dsCust1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 500);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.lblLName);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.lblFName);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.dgResult);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsCust1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnName;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.DataGridView dgResult;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblFName;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.Label lblLName;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.TextBox txtCity;
+        private DsCust dsCust1;
+    }
+}
+
---
+++ Prashant/Session 14 (Core ADO.Net)/DBApp/DBApp/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {
+            //RunExecuteScalar();
+            //RunExecuteReader();          
+            //AddCustomer();
+            //UpdateCustomer(1);
+            //DeleteCustomer(3);
+            RunExecuteDatasetForExcel();
+        }
+
+        private void DeleteCustomer(int id)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=PRASHANT-K\SQLEXPRESS;Initial Catalog=SampleDB;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+
+            con.Open();
+            cmd.CommandText = "DELETE FROM [table] where id = " + id;
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+        private void UpdateCustomer(int id)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=PRASHANT-K\SQLEXPRESS;Initial Catalog=SampleDB;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+
+            con.Open();
+            cmd.CommandText = "UPDATE [table] SET FName = '" + txtFName.Text +"', LName = '" +txtLName.Text + "',City = '" +  txtCity.Text + "' where id = " + id;
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+        private void AddCustomer()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=PRASHANT-K\SQLEXPRESS;Initial Catalog=SampleDB;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+
+            con.Open();
+
+            cmd.CommandText = "Select max(id) from [table]";
+            int id = Int32.Parse(cmd.ExecuteScalar().ToString());
+            id++;
+
+            cmd.CommandText = "INSERT INTO [table] (id, FName,LName, City) VALUES ("+ id  + ",'"+ txtFName.Text +"','" +txtLName.Text +"','" + txtCity.Text + "')";
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+         
+        }
+
+
+        private void RunExecuteScalar()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=PRASHANT-K\SQLEXPRESS;Initial Catalog=SampleDB;Integrated Security=True");
+            con.Open();
+            using (con)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText ="Select City from [table]";
+                cmd.CommandType = CommandType.Text;
+
+                var city = cmd.ExecuteScalar();
+                txtResult.Text = city.ToString();
+            }
+        }
+
+        private void RunExecuteDataset()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=PRASHANT-K\SQLEXPRESS;Initial Catalog=SampleDB;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;         
+            cmd.CommandText = "Select * from [table]";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet dsCustomer = new DataSet();
+            adapter.Fill(dsCustomer);
+
+            dgResult.DataSource = dsCustomer;
+            dgResult.DataMember = dsCustomer.Tables[0].TableName;
+        }
+
+        private void RunExecuteDatasetForExcel()
+        {
+             OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\Customer.xls;Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'");
+
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "Select * from [Sheet1$]";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+            adapter.Fill(dsCust1);
+        
+            dgResult.DataSource = dsCust1;
+            dgResult.DataMember = dsCust1.Tables[0].TableName;
+        }
+
+        private void RunExecuteReader()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=PRASHANT-K\SQLEXPRESS;Initial Catalog=SampleDB;Integrated Security=True");
+            con.Open();
+            using (con)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "Select * from [table]";
+                cmd.CommandType = CommandType.Text;
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    con.Close();
+                    txtResult.Text = reader[1] + " " + reader[2];
+                }
+            }
+        }
+    }
+}
